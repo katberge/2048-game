9d0a5af98b466a587e85272bda95a4a9bc87976a
Kat Berge <kat.n.berge@gmail.com>
2020-07-04 10:49:53 -0500

    creates a way to start game and draw new squares (2 squares to start the game)

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector("#grid");
-    let cells = Array.from(document.querySelector("#grid giv"));
+    let squares = Array.from(document.querySelectorAll("#grid div"));
     const width = 4;
+    const numbers = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+
+    // add new square to the grid
+    function newSquare() {
+        let randomPosition = Math.floor(Math.random() * width * width);
+        while (squares[randomPosition].classList.contains("taken")) {
+            randomPosition = Math.floor(Math.random() * width * width);
+        }
+        squares[randomPosition].classList.add("taken");
+    }
+
+    // start game
+    function startGame() {
+        newSquare();
+        newSquare();
+    }
+    // have game start right away
+    startGame();
 })
