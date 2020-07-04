b9df428ff83a66bd357b07b1bf866699143d2e3d
Kat Berge <kat.n.berge@gmail.com>
2020-07-04 10:00:51 -0500

    adds initial set up for js

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.querySelector("#grid");
+    let cells = Array.from(document.querySelector("#grid giv"));
+    const width = 4;
+})
